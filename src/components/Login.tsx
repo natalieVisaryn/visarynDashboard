4de0adc4c5dc9375b1c9748e1577a03026bbc672
Natalie Gereluk <natalie@seriesfi.com>
2026-01-26 18:33:06 -0700

    fixing build errors

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useState, FormEvent, useEffect } from 'react';
+import { useState, useEffect, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { login, hasCookie } from '../utils/auth';
 
